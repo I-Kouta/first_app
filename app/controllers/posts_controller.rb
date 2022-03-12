31476c0ea60258f389af648557587d3769b2620d
I-Kouta <kouta.baseball0222@gmail.com>
2022-03-12 11:45:02 +0900

    コメントの追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,6 +7,6 @@ class PostsController < ApplicationController
   end
 
   def create
-    Post.create(content: params[:content])
+    Post.create(content: params[:content]) # カラム名と送られてきたデータ
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   get 'posts', to: 'posts#index'
-  get 'posts/new', to: 'posts#new'
+  get 'posts/new', to: 'posts#new' # postコントローラーのnewアクションに進む
   post 'posts', to: 'posts#create'
 end
